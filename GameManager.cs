724b799c965c75cb76d0c9b79ff95eaf1c58d50b
ebba1122 <47720850+ebba1122@users.noreply.github.com>
2019-03-20 10:25:08 +0100

    Add files via upload
    Assets>Sprites

---
+++ GameManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class GameManager : MonoBehaviour {
+
+
+    public Tilemap Darkmap;
+    public Tilemap Blurredmap;
+    public Tilemap Backgroundmap;
+
+    public Tile Darktile;
+    public Tile Blurredtile;
+
+    void Start()
+    {
+        Darkmap.origin = Blurredmap.origin = Backgroundmap.origin;
+        Darkmap.size = Blurredmap.size = Backgroundmap.size;
+    
+        foreach (Vector3Int p in Darkmap.cellBounds.allPositionsWithin)
+        {
+            Darkmap.SetTile(p, Darktile);
+        }
+
+        foreach (Vector3Int p in Blurredmap.cellBounds.allPositionsWithin)
+        {
+            Blurredmap.SetTile(p, Blurredtile);
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Switch.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch : MonoBehaviour
+{
+    [SerializeField]
+    GameObject SwitchOn;
+
+    [SerializeField]
+    GameObject SwitchOff;
+
+    public bool isOn = false;
+
+    void Start()
+    {
+        gameObject.GetComponent<SpriteRenderer>().sprite = SwitchOff.GetComponent<SpriteRenderer>().sprite;
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        gameObject.GetComponent<SpriteRenderer>().sprite = SwitchOn.GetComponent<SpriteRenderer>().sprite;
+
+        isOn = true;
+    }
+}
